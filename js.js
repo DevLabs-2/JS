0c1e15a61d14017e9146e8be65cbba4df3ab440a
MAXIMILIANO SHER <95596083@ort>
2023-09-05 12:11:51 -0300

    segunda act

--- js.js
+++ js.js
@@ -1,13 +1,55 @@
-function nombreCompleto(nombre,apellido){
-    return nombre+" "+apellido
+const cuentas = [
+    {
+        nombre: 'Martin Portnoy',
+        cbu: 2345,
+        saldo: 10
+    },
+    {
+        nombre: 'Santiago Ferrara',
+        cbu: 2346,
+        saldo: 20
+    },
+    {
+        nombre: 'Julián Lifschitz',
+        cbu: 7,
+        saldo: 847321
+    },
+    {
+        nombre: 'Luca Listingart',
+        cbu: 985,
+        saldo: 9
+    },
+    {
+        nombre: 'Mariana Maekanehisa',
+        cbu: 1,
+        saldo: 5939720
+    }
+];
+let saldoTotal = 0;
+for(const cuenta of cuentas){
+    saldoTotal += cuenta.saldo;
 }
-const persona = new Object();
-persona.nombre = "Juan";
-persona.apellido = "Martinez";
-persona.edad = 45;
-persona.ultimoLogin = Date();
-persona.vivo = true;
-persona.nombreCompleto = nombreCompleto(persona.nombre,persona.apellido);
-for(let propiedad in persona){
-    console.log(`${propiedad} : ${persona[propiedad]}`)
+console.log(`El promedio de saldo es: ${saldoTotal/cuentas.length}`)
+
+const MayorDiez = cuentas.filter(cuenta => cuenta.saldo > 10000)
+console.log(`Los cbus de la gente com +10,000 en su cuenta son:`)
+for(const cuenta of MayorDiez){
+    console.log(cuenta.cbu)
 }
+    let cbu = prompt("Buscar cuenta por CBU")
+for(const cuenta of cuentas){
+    if(cbu == cuenta.cbu){
+        for(const propiedad in cuenta){
+            console.log(cuenta[propiedad])
+        }
+    }
+}
+let i = 0;
+let cbuDel = prompt("Eliminar cuenta por CBU")
+for(const cuenta of cuentas){
+    if(cbu == cuenta.cbu){
+       cuentas.splice(i,1)
+    }
+    i++;
+}
+
